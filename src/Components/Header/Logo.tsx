e8088c94d96549c72a8da93a4393ca278b5e61f2
Siam Ahnaf <siamahnaf198@gmail.com>
2024-05-04 19:47:03 +0600

    Adding Beta Tags

--- src/Components/Header/Logo.tsx
+++ src/Components/Header/Logo.tsx
@@ -4,8 +4,9 @@ import Link from "next/link";
 const Logo = () => {
     return (
         <div>
-            <Link href="/">
+            <Link href="/" className="relative">
                 <Image src="/logo-main.png" alt="Webinuxs" width={700} height={700} className="w-[90px] -mt-5" />
+                <p className="text-xs bg-primary absolute top-0 right-0 px-1 py-px rounded-sm text-white">Beta</p>
             </Link>
         </div>
     );
