bdd51cad539f685bcef9a75878dde97623f7b8a4
Siam Ahnaf <siamahnaf198@gmail.com>
2024-05-04 22:50:49 +0600

    Adding Some Small Tricks!

--- src/Components/Header/Logo.tsx
+++ src/Components/Header/Logo.tsx
@@ -6,7 +6,7 @@ const Logo = () => {
         <div>
             <Link href="/" className="relative">
                 <Image src="/logo-main.png" alt="Webinuxs" width={700} height={700} className="w-[90px] -mt-5" />
-                <p className="text-xs bg-primary absolute top-0 right-0 px-1 py-px rounded-sm text-white">Beta</p>
+                <p className="text-xs bg-primary absolute top-0 right-0 px-1 py-px rounded text-white">Beta</p>
             </Link>
         </div>
     );
--- src/Components/Service/ProductContent/BestSelling.tsx
+++ src/Components/Service/ProductContent/BestSelling.tsx
@@ -32,7 +32,7 @@ const BestSelling = () => {
         <div className="mt-14">
             <div className="mb-8 w-[35%] 2xl:w-[35%] xl:w-[50%] lg:w-[65%] xxs:w-full">
                 <h4 className="text-2xl font-bold mb-2">Similar <span className="text-main">Product</span></h4>
-                <p className="text-[15px]">Welcome to our FAQ section, where we shed light on the most common queries and offer insights into our process and services. We believe that transparency and clarity are key to building trust with our clients.</p>
+                <p className="text-[15px]">Similar suggested services you may choose.</p>
             </div>
             {isFetching &&
                 <div className="mt-2">
